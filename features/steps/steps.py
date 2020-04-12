d626683bdaa9a281d1d1a1472af4713fe5db701b
Jacks <jacksesmer@hotmail.com>
2020-04-12 15:48:43 -0300

    Added more tests

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -10,6 +10,7 @@ import page
 @given('we open page using "{browser}"')
 def setUp(context, browser):
     global driver
+    global main_page
     if 'ie' in browser.lower():
         driver = webdriver.Ie(IEDriverManager().install())
     elif 'firefox' in browser.lower():
@@ -20,23 +21,68 @@ def setUp(context, browser):
         driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.implicitly_wait(5)
     driver.get("https://stonepayments.typeform.com/to/yl5PKW")
+    main_page = page.MainPage(driver)
     pass
 
 
-@when('we fill form')
-def qa_steps(context):
+@when('we fill all steps form')
+def happy_way(context):
     """
     steps of test, the button steps uses data-qa content and the others uses ID
     """
-
-    main_page = page.MainPage(driver)
     assert main_page.is_title_matches(), "Check the url."
     main_page.click_go_button('start-button')
-    main_page.fill_form('jacks', 'shortText-01E0CZKHBB2W6J8DAG90FYW2TV')
-    main_page.fill_form('esmer', 'shortText-01E0CZKHBBP4SACR3YZ3NYWC9X')
-    main_page.fill_form('teste@teste.com', 'email-01E0CZKHBBJRN4RP11HBPARBTY')
-    main_page.list_select('Brazil', 'oj55rDLBfuFE')
+    main_page.fill_form('jacks', 'shortText-01E0CZKHBB2W6J8DAG90FYW2TV', 'ok')
+    main_page.fill_form('esmer', 'shortText-01E0CZKHBBP4SACR3YZ3NYWC9X', 'ok')
+    main_page.fill_form('teste@teste.com', 'email-01E0CZKHBBJRN4RP11HBPARBTY', 'ok')
+    main_page.list_select('Brazil', 'oj55rDLBfuFE', 'ok')
     main_page.click_go_button('submit-button deep-purple-submit-button')
+
+
+@when('we put number in name')
+def name(context):
+    main_page.click_go_button('start-button')
+
+
+@when('we see a name error message')
+def error_name(context):
+    main_page.fill_form('1111', 'shortText-01E0CZKHBB2W6J8DAG90FYW2TV', 'error')
+
+
+@when('we put number in lastname')
+def lastname(context):
+    main_page.click_go_button('start-button')
+    main_page.fill_form('jacks', 'shortText-01E0CZKHBB2W6J8DAG90FYW2TV', 'ok')
+
+
+@when('we see a lastname error message')
+def error_lastname(context):
+    main_page.fill_form('1111', 'shortText-01E0CZKHBBP4SACR3YZ3NYWC9X', 'error')
+
+
+@when('we put wrong email')
+def email(context):
+    main_page.click_go_button('start-button')
+    main_page.fill_form('jacks', 'shortText-01E0CZKHBB2W6J8DAG90FYW2TV', 'ok')
+    main_page.fill_form('esmer', 'shortText-01E0CZKHBBP4SACR3YZ3NYWC9X', 'ok')
+
+
+@when('we see a email error message')
+def error_email(context):
+    main_page.fill_form('1111', 'email-01E0CZKHBBJRN4RP11HBPARBTY', 'error')
+
+
+@when('we put wrong country')
+def country(context):
+    main_page.click_go_button('start-button')
+    main_page.fill_form('jacks', 'shortText-01E0CZKHBB2W6J8DAG90FYW2TV', 'ok')
+    main_page.fill_form('esmer', 'shortText-01E0CZKHBBP4SACR3YZ3NYWC9X', 'ok')
+    main_page.fill_form('teste@teste.com', 'email-01E0CZKHBBJRN4RP11HBPARBTY', 'ok')
+
+
+@when('we see a invalid country message')
+def error_country(context):
+    main_page.list_select('11111', 'oj55rDLBfuFE', 'error')
 
 
 @then('we close webdriver')
